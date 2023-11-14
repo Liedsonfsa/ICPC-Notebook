7cee1be494fbcee79905efca06731ff993301749
Fabrício Liedson <111002008+Liedsonfsa@users.noreply.github.com>
2023-11-14 09:59:58 -0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _ ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define pb push_back
+#define ll long long
+#define f first
+#define s second
+#define ALL(x) x.begin(), x.end()
+#define RALL(x) x.rbegin(), x.rend()
+
+const int mod = 1e9+7;
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vii;
+
+int main() { _
+
+    
+
+    return 0;
+}
